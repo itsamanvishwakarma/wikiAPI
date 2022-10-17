3dbfe6bd5931333b4f5f9c7dd47afc639fe48ad1
Aman Vishwakarma <vishwakarmaaman8000@gmail.com>
2022-10-17 17:53:27 +0530

    chained route handler, problem in .put method yet to debug

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function (req, res) {
+///////////////////////////// Requests Targetting All Articles /////////////////////////////
+
+app.route("/articles")
+.get(function (req, res) {
   Article.find(function (err, foundArticles) {
     if (!err) {
       res.send(foundArticles);
@@ -29,6 +32,60 @@ app.get("/articles", function (req, res) {
       res.send(err);
     }
   });
+})
+
+.post(function (req, res) {
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  newArticle.save(function (err) {
+    if (!err) {
+      res.send("Successfully added a new article.");
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+.delete(function (req, res) {
+  Article.deleteMany(function (err) {
+    if (!err) {
+      res.send("Successfully deleted all articles.");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+///////////////////////////// Requests Targetting A Specific Article /////////////////////////////
+
+app.route("/articles/:articleTitle")
+
+.get(function (req, res) {
+  Article.findOne(
+    { title: req.params.articleTitle },
+    function (err, foundArticle) {
+      if (foundArticle) {
+        res.send(foundArticle);
+      } else {
+        res.send("No articles matching that title was found.");
+      }
+    }
+  );
+})
+
+.put(function (req, res) {
+  Article.updateOne(
+    { title: req.params.articleTitle },
+    { title: req.body.title, content: req.body.content },
+    { overwrite: true },
+    function (err) {
+      if (!err) {
+        res.send("Successfully updated article.");
+      }
+    }
+  );
 });
 
 app.listen(3000, function () {
